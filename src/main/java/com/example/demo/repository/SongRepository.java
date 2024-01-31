9edd301a655d52174218f3e3b145f40ecbf3dc06
PILLIPAKA VARALAKSHM <Varupillipaka@gmail.com>
2024-01-31 15:40:49 +0530

    1st version of tunehub

---
+++ src/main/java/com/example/demo/repository/SongRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Song;
+
+public interface SongRepository extends JpaRepository<Song, Integer> {
+   public Song findByName(String name);
+}
